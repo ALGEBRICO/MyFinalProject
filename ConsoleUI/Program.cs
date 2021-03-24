c7e2ee9a8a1606d2c7d9f27804e32254586108fc
Nihan-PC <Nihan-PC@DESKTOP-2SENHO0>
2021-03-25 02:16:21 +0300

    25.03.2021 Last Version17.02.2021 Şubat  Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,16 +5,46 @@ using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            //ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAll())
+            //Data Transformation Object
+            ProductTest();
+            //IoC 
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll().Data)
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(category.CategoryName);
             }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal()
+                ,new CategoryManager(new EfCategoryDal()));
+
+            var result = productManager.GetProductDetails();
+
+            if (result.Success==true)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
             
         }
     }
