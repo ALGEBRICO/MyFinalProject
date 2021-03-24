57dfaab4f20e4c9277b8e65fbe2b42c22ba05c54
Nihan-PC <Nihan-PC@DESKTOP-2SENHO0>
2021-03-25 02:28:25 +0300

    25.03.2021 LAST Version

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 
 namespace Core.Extensions
 {
-    public class ErrorDetails
+    public class ErrorDetails  
     {
         public string Message { get; set; }
         public int StatusCode { get; set; }
